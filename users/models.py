925616c0cead421f940ea833c479333003da1960
Anton Fomenko <antonfomenko1234@gmail.com>
2025-03-13 20:04:59 +0300

    create lms app. create models User, Course, Lesson

--- users/models.py
+++ users/models.py
@@ -3,4 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона')
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name='Страна')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватар')
+    # token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    # is_blocked = models.BooleanField(default=False, verbose_name="Пользователь заблокирован")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
